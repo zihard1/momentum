670d8ede9df72b5262f8f928ecaab8d6dc8f0f9d
hclee <zihard1@naver.com>
2022-01-21 12:27:14 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+
+console.dir(h1);
+
+function handleH1Click() {
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
+}
+
+function handleMouseEnter() {
+    h1.style.cursor = "pointer";
+}
+
+h1.addEventListener("click", handleH1Click);
+h1.addEventListener("mouseenter", handleMouseEnter);
